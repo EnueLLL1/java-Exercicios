9bf080db307cc4e5f8e1efc0de6f5e24a424b792
EnueLLL1 <Eg121745@gmail.com>
2025-05-24 14:13:41 -0300

    Corrige a leitura do nome do aluno e adiciona validações para as notas, além de melhorar a mensagem de retorno da nota final.

--- Lista02Exe04/src/executavel/Apresentacao.java
+++ Lista02Exe04/src/executavel/Apresentacao.java
@@ -11,23 +11,38 @@ public class Apresentacao {
 		System.out.println("BEM VINDO AO PROGRAMA DE NOTAS");
 		
 		System.out.print("ADICIONE O NOME DO ALUNO: ");
-		String nome = lt.next();
+		String nome = lt.nextLine();
 		
 		System.out.println("ADCIONE A PRIMEIRA NOTA SENDO NO MAXIMO 30");
 		double nt01 = lt.nextDouble();
 		lt.nextLine();
+			// Verifica se a nota é maior que 30
+			if (nt01 > 30) {
+				System.out.println("A primeira nota não pode ser maior que 30");
+				nt01 = 0;
+				}
 
 		System.out.println("ADCIONE A SEGUNDA NOTA SENDO NO MAXIMO 35");
 		double nt02 = lt.nextDouble();		
 		lt.nextLine();
+			// Verifica se a nota é maior que 35
+			if (nt02 > 35) {
+				System.out.println("A segunda nota não pode ser maior que 35");
+				nt02 = 0;
+				}
 
 		System.out.println("ADCIONE A TERCEIRA NOTA SENDO NO MAXIMO 35");
 		double nt03 = lt.nextDouble();
 		lt.nextLine();
+			// Verifica se a nota é maior que 35
+			if (nt03 > 35) {
+				System.out.println("A terceira nota não pode ser maior que 35");
+				nt03 = 0;
+				}
 
-		Aluno al1 = new Aluno(nome,nt01,nt02,nt03);
+		Aluno notasAluno = new Aluno(nome,nt01,nt02,nt03);
 		
-		System.out.println(al1.ntF());
+		System.out.println(notasAluno.ntF());
 		lt.close();
 	}
 
--- Lista02Exe04/src/notas/Aluno.java
+++ Lista02Exe04/src/notas/Aluno.java
@@ -21,12 +21,11 @@ public class Aluno {
 
 	public String ntF() {
 		if (ntF >= 60) {
-			return "O Aluno " + nome + " foi Aprovado com a nota final " + ntF + " Tendo as segintes notas, Nota 1:" + nt01 + ", Nota 2 :"+ nt02 + ", é Nota 3:" + nt03;
+			return "Nota final " + ntF + "\nAprovado";
 		} else {
 			double pF;
 			pF = 60 - ntF;
-			return "Falta pouco, a sua nota final é " + ntF + ", e os pontos faltantes são " + pF
-					+ " e para ser aprovado precisa de 60.";
+			return "Nota Final: " + ntF + "\nReprovado\nFaltaram " + pF +" pontos para ser aprovado";
 
 		}
 	}
